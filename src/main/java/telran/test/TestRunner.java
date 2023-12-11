fe4afdbfac214a149efaa3266cc3ffec71fd8d2e
Evgeny Pankov <volvovnl123@gmail.com>
2023-12-11 20:21:25 +0700

    HW #54 applying @beforeeach and @test annotations

--- src/main/java/telran/test/TestRunner.java
+++ src/main/java/telran/test/TestRunner.java
@@ -2,8 +2,9 @@ package telran.test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
-import telran.test.annotation.Test;
+import telran.test.annotation.*;
 
 public class TestRunner implements Runnable {
 	private Object testObj;
@@ -16,16 +17,37 @@ public class TestRunner implements Runnable {
 	public void run() {
 		Class<?> clazz = testObj.getClass();
 		Method[] methods = clazz.getDeclaredMethods();
+		Method[] methodBeforeEach = runMethodBeforeEach(methods);
+		runTestMethod(methods, methodBeforeEach);
+	}
+
+	private Method[] runMethodBeforeEach(Method[] methods) {
+		return Arrays.stream(methods).filter(m -> m.isAnnotationPresent(BeforeEach.class)).toArray(Method[]::new);
+	}
+
+	private void runTestMethod(Method[] methods, Method[] methodBeforeEach) {
 		for (Method method : methods) {
 			if (method.isAnnotationPresent(Test.class)) {
 				method.setAccessible(true);
 				try {
+					runMethods(methodBeforeEach);
 					method.invoke(testObj);
 				} catch (IllegalAccessException | InvocationTargetException e) {
 					System.out.println("error: " + e.getMessage());
 				}
 			}
 		}
-
 	}
+
+	private void runMethods(Method[] methodBeforeEach) {
+		for (Method method : methodBeforeEach) {
+			method.setAccessible(true);
+			try {
+				method.invoke(testObj);
+			} catch (IllegalAccessException | InvocationTargetException e) {
+				System.out.println("error: " + e.getMessage());
+			}
+		}
+	}
+
 }
