80fe22fa3a649a0c326f199d6751d05ac32cd663
Evgeny Pankov <volvovnl123@gmail.com>
2023-12-11 19:27:38 +0700

    CW #54 annotation Test and its processing

---
+++ src/main/java/telran/test/TestRunner.java
@@ -0,0 +1,31 @@
+package telran.test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import telran.test.annotation.Test;
+
+public class TestRunner implements Runnable {
+	private Object testObj;
+
+	public TestRunner(Object testObj) {
+		this.testObj = testObj;
+	}
+
+	@Override
+	public void run() {
+		Class<?> clazz = testObj.getClass();
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			if (method.isAnnotationPresent(Test.class)) {
+				method.setAccessible(true);
+				try {
+					method.invoke(testObj);
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					System.out.println("error: " + e.getMessage());
+				}
+			}
+		}
+
+	}
+}
